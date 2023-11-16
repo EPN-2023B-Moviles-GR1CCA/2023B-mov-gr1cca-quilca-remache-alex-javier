04ea6a458132a35999df13c19e5b2d8bed97c44c
ALEXaqr <alex.quilca@epn.edu.ec>
2023-11-16 18:04:32 -0500

    hasta constructor secundario

--- 01-kotlin/Proyecto2023bgr1acc/src/main/kotlin/Main.kt
+++ 01-kotlin/Proyecto2023bgr1acc/src/main/kotlin/Main.kt
@@ -58,38 +58,65 @@ fun main(){
 //COnstructor primario se pone despues de poner la clase
 //init --> bloque de codigo del constructor primario
 
-abstract class NUmerosJava{
+abstract class NumerosJava{
     protected val numeroUno: Int
     private val numeroDos: Int
-
     constructor(
         uno: Int,
         dos: Int
-    ){//Bloque de codigo del constructor
+    ){ // Bloque de codigo del constructor
         this.numeroUno = uno
         this.numeroDos = dos
+        println("Inicializando")
+    }
+}
+abstract class Numeros( // Constructor PRIMARIO
+    // Ejemplo:
+    // unoProp: Int, // (Parametro (sin modificador de acceso))
+    // private var uno: Int, // Propiedad Publica Clase numeros.uno
+    // var uno: Int, // Propiedad de la clase (por defecto es PUBLIC)
+    // public var uno: Int,
+    // Propiedad de la clase protected numeros.numeroUno
+    protected val numeroUno: Int,
+    // Propiedad de la clase protected numeros.numeroDos
+    protected val numeroDos: Int,
+){
+    // var cedula: string = "" (public es por defecto)
+    // private valorCalculado: Int = 0 (private)
+    init { // Bloque codigo constructor primario
+        this.numeroUno; this.numeroDos; // this es opcional
+        numeroUno; numeroDos; // sin el "this", es lo mismo
+        println("Inicializando")
     }
 }
 
-abstract class Numeros(// CONTRUCTOR PRIMARIO
-    //Ejemplo:
-    //unoProp: Int, //(parametro (sin modificador de acceso))
-    //private var uno: Int, //propiedd publica clase numeros.uno
-    //var uno: Int, //Propiedad de la clase (por defecto es PUBLIC)
-    //public var uno: Int,
-    //Propiedad de la clase protected numeros.numeroUno
-    protected val numeroUno: Int,
-    //Propiedad de la clase protected numeros,numeroDos
-    protected val numeroDos: Int,
-){
-    //var cedula: string = "" (public es por defecto)
-    //private valorCalculado: Int = 0 (private)
-
-    init {//Bloque codifo contructor primario
-        this.numeroUno; this.numeroDos; //this es opcional
-        numeroUno; numeroDos;// sin el this es lo mismo
-        println("Inicializando")
+class Suma( // Constructor Primario Suma
+    uno: Int, // Parametro
+    dos: Int // Parametro
+): Numeros(uno, dos) { // <- Constructor del Padre
+    init { // Bloque constructor primario
+        this.numeroUno; numeroUno;
+        this.numeroDos; numeroDos;
     }
+
+    constructor(//  Segundo constructor
+        uno: Int?, // parametros
+        dos: Int // parametros
+    ) : this(  // llamada constructor primario
+        if (uno == null) 0 else uno,
+        dos
+    ) { // si necesitamos bloque de codigo lo usamos
+        numeroUno;
+    }
+
+    constructor(//  tercer constructor
+        uno: Int, // parametros
+        dos: Int? // parametros
+    ) : this(  // llamada constructor primario
+        uno,
+        if (dos == null) 0 else uno
+    )
+    // Si no lo necesitamos al bloque de codigo "{}" lo omitimos
 }
 
 //FuncionesSS
