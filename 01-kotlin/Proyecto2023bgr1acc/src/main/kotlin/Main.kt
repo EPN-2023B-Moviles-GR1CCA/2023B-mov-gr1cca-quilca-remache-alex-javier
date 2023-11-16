f740f8d7be11dccb1b83425fc0aa2fd1cea5a53f
ALEXaqr <alex.quilca@epn.edu.ec>
2023-11-16 17:02:36 -0500

    hasta funcion calcular sueldo

--- 01-kotlin/Proyecto2023bgr1acc/src/main/kotlin/Main.kt
+++ 01-kotlin/Proyecto2023bgr1acc/src/main/kotlin/Main.kt
@@ -31,6 +31,35 @@ fun main(){
 
     val coqueteo = if (estadoCivilWhen == "S") "Si" else "No"
 
+
+
+
+
     //Classes Java
     val fechaNacimiento: Date = Date()
+}
+
+//Funciones
+//Unit --> void en kotlin
+//Double? --> ayuda al programado que la variable puede ser null, a este tipo de variable se lo llama nullable
+// se puede hacer nullable a cualquier tipo de variable
+// void -> Unit
+fun imprimirNombre(nombre: String): Unit{
+    // "Nombre: " + variable + " bienvenido";
+    println("Nombre : ${nombre}")
+}
+fun calcularSueldo(
+    sueldo: Double, // Requerido
+    tasa: Double = 12.00, // Opcional (defecto)
+    bonoEspecial: Double? = null, // Opcion null -> nullable
+): Double{
+    // Int -> Int? (nullable)
+    // String -> String? (nullable)
+    // Date -> Date? (nullable)
+    if(bonoEspecial == null){
+        return sueldo * (100/tasa)
+    }else{
+        bonoEspecial.dec()
+        return sueldo * (100/tasa ) + bonoEspecial
+    }
 }
