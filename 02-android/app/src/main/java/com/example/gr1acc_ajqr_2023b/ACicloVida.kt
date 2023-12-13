099132d7a5a5df98e6f4466ddbe0e5ed5b61b608
ALEXaqr <alex.quilca@epn.edu.ec>
2023-12-13 17:37:02 -0500

    Creacion actividad ciclo de vida

---
+++ 02-android/app/src/main/java/com/example/gr1acc_ajqr_2023b/ACicloVida.kt
@@ -0,0 +1,15 @@
+package com.example.gr1acc_ajqr_2023b
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ACicloVida : AppCompatActivity() {
+
+    
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_aciclo_vida)
+    }
+}
