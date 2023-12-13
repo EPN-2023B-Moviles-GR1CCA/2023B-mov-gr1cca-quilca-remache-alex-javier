868653fd74cadf4b4d828ab4bc0ddc82b61d3311
ALEXaqr <alex.quilca@epn.edu.ec>
2023-12-13 17:43:41 -0500

    Hasta mostrarSnackbar en ACicloVida

--- 02-android/app/src/main/java/com/example/gr1acc_ajqr_2023b/ACicloVida.kt
+++ 02-android/app/src/main/java/com/example/gr1acc_ajqr_2023b/ACicloVida.kt
@@ -2,10 +2,22 @@ package com.example.gr1acc_ajqr_2023b
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
 
 class ACicloVida : AppCompatActivity() {
+    var textoGlobal = ""
 
-    
+    fun mostrarSnackbar(texto:String){
+        textoGlobal = textoGlobal + " " + texto
+        Snackbar
+            .make(
+                findViewById(R.id.cl_ciclo_vida), //view
+                textoGlobal, //texto
+                Snackbar.LENGTH_INDEFINITE //tiempo
+            )
+            .show()
+    }
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
