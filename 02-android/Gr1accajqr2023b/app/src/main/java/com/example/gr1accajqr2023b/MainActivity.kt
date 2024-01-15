d17bda57f7cdf73afa034ef7d15fbda480ecbb39
ALEXaqr <alex.quilca@epn.edu.ec>
2024-01-15 15:19:58 -0500

    hasta callbacks

--- 02-android/Gr1accajqr2023b/app/src/main/java/com/example/gr1accajqr2023b/MainActivity.kt
+++ 02-android/Gr1accajqr2023b/app/src/main/java/com/example/gr1accajqr2023b/MainActivity.kt
@@ -1,12 +1,66 @@
 package com.example.gr1accajqr2023b
 
+import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.ContactsContract
 import android.widget.Button
 import androidx.activity.ComponentActivity
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.android.material.snackbar.Snackbar
 
 
 class MainActivity : ComponentActivity() {
+
+    val callbackContenidoIntentExplicito =
+        registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ){
+                result ->
+            if(result.resultCode == Activity.RESULT_OK){
+                if(result.data != null){
+                    // Logica Negocio
+                    val data = result.data
+                    mostrarSnackbar(
+                        "${data?.getStringExtra("nombreModificado")}"
+                    )
+                }
+            }
+        }
+    fun mostrarSnackbar(texto:String){
+        Snackbar
+            .make(
+                findViewById(R.id.id_layout_main), // view
+                texto, // texto
+                Snackbar.LENGTH_LONG // tiempo
+            )
+            .show()
+    }
+
+    val callbackIntentImplicitoTelefono =
+        registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ){
+                result ->
+            if(result.resultCode === Activity.RESULT_OK){
+                if(result.data != null){
+                    if(result.data!!.data != null){
+                        val uri: Uri = result.data!!.data!!
+                        val cursor = contentResolver.query(
+                            uri, null, null, null,  null, null)
+                        cursor?.moveToFirst()
+                        val indiceTelefono = cursor?.getColumnIndex(
+                            ContactsContract.CommonDataKinds.Phone.NUMBER
+                        )
+                        val telefono = cursor?.getString(indiceTelefono!!)
+                        cursor?.close()
+                        mostrarSnackbar("Telefono ${telefono}")
+                    }
+                }
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val botonACicloVida = findViewById<Button>(R.id.btn_ciclo_vida)
