8f7dc106a0a9de543723a5c535c431d08f016b61
ALEXaqr <alex.quilca@epn.edu.ec>
2024-01-14 23:29:42 -0500

    Hasta ir blist view

--- 02-android/Gr1accajqr2023b/app/src/main/java/com/example/gr1accajqr2023b/MainActivity.kt
+++ 02-android/Gr1accajqr2023b/app/src/main/java/com/example/gr1accajqr2023b/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.gr1accajqr2023b
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.ComponentActivity
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val botonACicloVida = findViewById<Button>(R.id.btn_ciclo_vida)
+        botonACicloVida
+            .setOnClickListener {
+                irActividad(ACicloVida::class.java)
+            }
+
+        val botonListView = findViewById<Button>(R.id.btn_ir_list_view)
+        botonListView
+            .setOnClickListener {
+                irActividad(BListView::class.java)
+            }
+    }
+    
+    fun irActividad(
+        clase: Class<*>
+    ){
+        val intent = Intent(this, clase)
+        startActivity(intent)
     }
 }
